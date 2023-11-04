5396e2bab04212e7231b7ae63f619a4ce0a55983
aminbsh <aminbashash93@gmail.com>
2023-11-04 20:21:53 +0330

    the project 13 created

---
+++ project 13.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int r, p;
+            int output;
+
+            r = Convert.ToInt32(Console.ReadLine());
+            p = Convert.ToInt32(Console.ReadLine());
+            output=power(r, p);
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+        static int power(int r,int p)
+        {
+            int f = r;
+
+            if(p==1)
+            {
+                return r;
+            }
+            r = power(r, p - 1);
+            r *= f;
+            return r;
+        }
+    }
+}
