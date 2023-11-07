8b17c7e8ebbd2fdbdcd33b9945102d3a99da28ec
Hamed <hammedlootf@gmail.com>
2023-11-07 17:26:28 +0330

    upload tarkib project

--- tarkib.cpp
+++ tarkib.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 using namespace std;
-int combination(int n ,int  m)
-{
-    if(n = m , m = 0)
-    return 1 ;
-    return combination(n-1 , m-1) + combination (n-1, m);
-
+int combination(int n, int m) {
+    if (m == 0 || m == n)
+        return 1;
+        return combination(n - 1, m - 1) + combination(n - 1, m);
 }
-int main()
-{
-    int n , m , result;
-    cout << "Enter n :" << endl;
-    cin >> n ;
-    cout << "enter m :" << endl;
-    cin >> m ;
-    cout << "your combination is : " << combination(n , m);
-
+int main() {
+    int n, m;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << "Enter the value of r: ";
+    cin >> m;
+    cout << "The combination of " << n << " and " << m << " is : " << combination(n, m) << endl;
+    return 0;
 }
+
