6bb027dcb5b91d56da7053fd2ae9c6bea9049bd1
Hamed <hammedlootf@gmail.com>
2023-11-09 11:01:21 +0330

    hanoi project

--- hanoi.cpp
+++ hanoi.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void hanoi(int n, char origin , char destination, char ancillary) {
+void hanoi(int n, char origin , char ancillary , char destination) {
     if (n == 1) {
         cout << "Move disk 1 from " << origin  << " to " << destination << endl;
         return;
     }
-    hanoi(n-1, origin , ancillary, destination);
+    hanoi(n-1, origin , destination , ancillary);
     cout << "Move disk " << n << " from " << origin  << " to " << destination << endl;
-    hanoi(n-1, ancillary, destination, origin );
+    hanoi(n-1, ancillary, origin ,destination );
 }
 int main() {
     int number_disks;
     cout << "Enter the number of disks: ";
     cin >> number_disks;
-    hanoi(number_disks, 'A', 'C', 'B');
+    hanoi(number_disks, 'A', 'B', 'C');
     return 0;
 }
 
